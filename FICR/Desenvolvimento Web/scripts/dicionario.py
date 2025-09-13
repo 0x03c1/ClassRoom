874750f7828c448f9a6a5271cc912a295574084c
ronierisonmaciel <ronierison.maciel@gmail.com>
2025-09-13 01:26:24 -0300

    feat: add dicionario.py with sample data and commented code examples

---
+++ FICR/Desenvolvimento Web/scripts/dicionario.py
@@ -0,0 +1,22 @@
+n_n = {'Maria':10, 'João':15, 'Josefa':24, 'Carlos':22}
+# print(n_n)
+
+# print(n_n['João'])
+# print(f'chave {n_n.keys()}')
+# print(f'valor {n_n.values()}')
+
+# adicionando elementos
+# n_n['Eduardo'] = 21
+
+# removendo chave João
+# del n_n['João']
+
+dados = {
+    'endereço': {'Rua da Paz':145, 'Caxangá':587, 'Rua da Aurora': 198},
+    'cadastro': {'Maria':10, 'João':15, 'Josefa':24, 'Carlos':22}
+    }
+
+# for k, v in n_n.items():
+#     print(f'Nome: {k} idade: {v}')
+
+print(dados)
