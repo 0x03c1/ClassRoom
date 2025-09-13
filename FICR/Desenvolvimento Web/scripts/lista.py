e4bc9a7daa153dd360d9c3acfa51c4ae9517a25b
ronierisonmaciel <ronierison.maciel@gmail.com>
2025-09-13 01:26:36 -0300

    feat: add lista.py with examples of list slicing and conditional statements

---
+++ FICR/Desenvolvimento Web/scripts/lista.py
@@ -0,0 +1,23 @@
+"""
+frutas = ['maçã', 'uva', 'banana', 'pera']
+
+if 'maçã' in frutas:
+    print('yes')
+else:
+    print('no')
+"""
+
+# quadrados = [x**2 for x in range (10)]
+# print(quadrados)
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(numeros [2:5]) # Saída: [2 , 3, 4]
+print(numeros [:4]) # Saída: [0 , 1 , 2 , 3]
+print(numeros [::2])
+
+[1, 3, 4, 5, 6, 7, 9, 7, 2, 3, 5, 6]
+
+
+
+
