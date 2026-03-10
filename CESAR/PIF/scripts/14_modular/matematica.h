6187ffe1b4f1865ba5776b65c7dfee7680110f6d
ronierisonmaciel <ronierison.maciel@gmail.com>
2026-03-10 18:45:33 -0300

    feat: add header file for mathematical functions declarations in C

---
+++ CESAR/PIF/scripts/14_modular/matematica.h
@@ -0,0 +1,9 @@
+#ifndef MATEMATICA_H    /* Include Guard: evita inclusão dupla */
+#define MATEMATICA_H
+
+/* Apenas DECLARAÇÕES (protótipos) */
+int    fatorial(int n);
+double potencia(double base, int exp);
+int    eh_primo(int n);
+
+#endif /* MATEMATICA_H */
