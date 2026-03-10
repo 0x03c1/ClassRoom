d14d7deacb0a6104dc9eade58b80d38f1758e3bb
ronierisonmaciel <ronierison.maciel@gmail.com>
2026-03-10 18:43:39 -0300

    feat: add hello world program in C

---
+++ CESAR/PIF/scripts/01_hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  /* Inclui a biblioteca de entrada/saída */
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;  /* Indica ao sistema que o programa terminou com sucesso */
+}
