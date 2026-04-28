70039fa10a79a266344b75e91df9e9f5570ea2fe
ronierisonmaciel <ronierison.maciel@gmail.com>
2026-04-28 18:51:58 -0300

    feat: remove deprecated activity scripts from the project

--- CESAR/PIF/scripts/atividades/01_hello_world.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>  /* Inclui a biblioteca de entrada/saída */
-
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;  /* Indica ao sistema que o programa terminou com sucesso */
-}
